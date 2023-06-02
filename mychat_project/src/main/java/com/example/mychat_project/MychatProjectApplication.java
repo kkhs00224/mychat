0f90cef6ae0ddbed4553bf4d9188981d5a2c3e3a
kkhs00224 <kkhs00224@gmail.com>
2023-06-02 18:34:24 +0900

    first commit

---
+++ mychat_project/src/main/java/com/example/mychat_project/MychatProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.mychat_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MychatProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MychatProjectApplication.class, args);
+    }
+
+}
---
+++ mychat_project/src/test/java/com/example/mychat_project/MychatProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mychat_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MychatProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
